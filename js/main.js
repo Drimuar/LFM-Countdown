af08fff9f7581bc3193f39f84fc1aa88df9c80fd
Drimuar <Drimuar@gmail.com>
2022-05-20 17:26:21 +0700

    delete redundancy

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ import { intervalToDuration, formatDuration, add } from 'date-fns';
 
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
+const timeZone = -7;
 
 input.addEventListener("input", timeCount);
 
 function timeCount() {
-	const date = add(new Date(input.value), { hours: -7 });
+	const date = add(new Date(input.value), { hours: timeZone });
 	const durationObject = intervalToDuration({ start: Date.now(), end: date });
-	const durationFormatedObject = {
+	let untilDate = formatDuration({
 		years: durationObject.years,
 		days: durationObject.days,
 		hours: durationObject.hours,
-	};
-	let untilDate = formatDuration(durationFormatedObject);
+	});
 	output.value = `${untilDate}`
 		+ " until " + format(date, "dd/MM/yyyy");
 }
