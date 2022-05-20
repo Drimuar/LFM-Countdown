6d802dd2384f5ba086cbe4f4550e9d3428155597
Drimuar <Drimuar@gmail.com>
2022-05-20 12:57:07 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const { format } = require("date-fns");
+import { formatDistanceToNowStrict, formatDuration, add } from 'date-fns';
+
+const input = document.querySelector('.input');
+const output = document.querySelector('.output');
+
+input.addEventListener("input", timeCount);
+
+function timeCount() {
+	const date = add(new Date(input.value), { hours: -7 });
+	let hour = formatDistanceToNowStrict(date, { unit: "hour" }).slice(0, -5);
+	let year = formatDistanceToNowStrict(date, { unit: "year" }).slice(0, -5);;
+	let day = formatDistanceToNowStrict(date, { unit: "day" }).slice(0, -4);;
+	let obj = {
+		years: +year,
+		days: +day,
+		hours: +hour % 24
+	};
+	let untilDate = formatDuration(obj);
+	output.value = `${untilDate}`
+		+ " until " + format(date, "dd/MM/yyyy");
+}
